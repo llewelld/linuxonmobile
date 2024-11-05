d949d51b5f33064398449e795aff8be9f53f0b90
David Llewellyn-Jones <david@flypig.co.uk>
2024-11-05 19:12:58 +0000

    Add the code for the website
    Adds the code for the website, README, LICENSE file and so on.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # linuxonmobile
+
 Linux on Mobile website
+
+https://www.linuxonmobile.net
+
+## Details
+
+Provides the website impolementatin for the Linux on Mobile Stand.
+
+This has been used as a launchpad for the Linux on Mobile stand at various events:
+1. FOSDEM 2024 in Brussels on 3-4 February 2024..
+2. DevConf.CZ in Brno on 13-15 June 2024. 
+
+## Licence
+
+Licensed under the GNU Affero Public License version 3.0.
+See the LICENSE file.
+
+## Contact
+
+Feel free to contact me for more info:
+
+David Llewellyn-Jones
+https://www.flpig.co.uk/
+
---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+  // The number of items to shuffle
+  const ELEMENT_NUM = 8;
+
+  // Swap two elements in the DOM
+  const swapElements = function (id1, id2) {
+    const content1 = $(id1).html();
+    const content2 = $(id2).html();
+    $(id1).html(content2);
+    $(id2).html(content1);
+  }
+
+  // Shuffle elements using Fisher-Yates, swapping DOM elements as we go
+  const shuffle = (array) => {
+    for (let count = array.length - 1; count > 0; count--) {
+      const swap = Math.floor(Math.random() * (count + 1));
+      swapElements(array[count], array[swap]);
+    }
+  };
+
+  // Build an array of elements to shuffle
+  var array = [];  
+  for (let count = 0; count < ELEMENT_NUM; ++count) {
+    array.push("#project" + count);
+  }
+
+  // Perform the shuffle
+  shuffle(array)
+});
